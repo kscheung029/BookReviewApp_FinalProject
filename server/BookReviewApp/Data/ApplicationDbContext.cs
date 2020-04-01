e50137c0f03892dc223bb889a4e0ea482633913f
Jinhong Liang <liangjinhongwin@hotmail.com>
2020-03-31 17:11:53 -0700

    initial commit

---
+++ server/BookReviewApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookReviewApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookReviewApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserBook> UserBooks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasKey(au => au.Id);
+
+            modelBuilder.Entity<UserBook>()
+                .HasKey(ub=> new { ub.Id, ub.UserId });
+
+            modelBuilder.Entity<UserBook>()
+                .HasOne(ub => ub.ApplicationUser)
+                .WithMany(au => au.UserBooks)
+                .HasForeignKey(ub => ub.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ server/BookReviewApp/Models/UserBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReviewApp.Models
+{
+    public class UserBook
+    {
+        public string Id { get; set; }
+        public string Review { get; set; }     
+        public DateTime ReviewedOn { get; set; }
+        public bool IsFavourite { get; set; }
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
